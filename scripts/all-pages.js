8e6de66ec196ef9a4ce930af8b2ddb768abd61da
Zandile Gebuza <2562617@students.wits.ac.za>
2024-09-16 15:14:52 +0200

    Update all-pages.js

--- scripts/all-pages.js
+++ scripts/all-pages.js
@@ -1,4 +1,4 @@
-//Nav Menu
+/*//Nav Menu
 const root = "/WSOA3029A_2562617"
 
 //An array of all the menu tabs
@@ -6,10 +6,22 @@ const menuItems = [
     { name: "Home", href: root + "/index.html" },
     { name: "About", href: `${root}/about/about.html` },
     { name: "Theory", href: `${root}/theory/theory.html` },
-    { name: "Essay", href: `${root}/essay/essay.html` },
+    { name: "Essay", href: `${root}/theory/essay.html` },
     { name: "Design", href: `${root}/design/design.html` },
     { name: "Content", href: `${root}/content/content.html` },
     { name: "Data-Visualisations", href: `${root}/data-visualisations/data-visualisations.html` },
+]*/
+
+//Nav Menu
+//An array of all the menu tabs
+const menuItems = [
+    { name: "Home", href: "/index.html" },
+    { name: "About", href: `/about/about.html` },
+    { name: "Theory", href: `/theory/theory.html` },
+    { name: "Essay", href: `/theory/essay.html` },
+    { name: "Design", href: `/design/design.html` },
+    { name: "Content", href: `/content/content.html` },
+    { name: "Data-Visualisations", href: `/data-visualisations/data-visualisations.html` },
 ]
 
 //Function for the nav menu
