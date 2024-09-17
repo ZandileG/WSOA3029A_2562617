9f3527292400f12928afd17ae9e87bef8c65ffd2
Zandile Gebuza <2562617@students.wits.ac.za>
2024-09-17 08:32:28 +0200

    Edits

--- scripts/data-visualisations.js
+++ scripts/data-visualisations.js
@@ -1,3 +1,7 @@
 /*https://api.tvmaze.com/singlesearch/shows?q=brooklyn+nine-nine&embed=episodes
 https://api.tvmaze.com/shows/49/seasons
 https://api.tvmaze.com/shows/49/episodes*/
+
+/*Two interactive data visualizations (different types of visuals). 
+The data visualizations should be surrounded by their appropriate context (UI/UX), 
+e.g.: tooltips, legends, maps, keys, explanations/expansion text, titles, etc. */
