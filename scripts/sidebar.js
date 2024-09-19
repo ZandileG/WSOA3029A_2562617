33e5d8e517360fb7feb7de4a036454bdc248ccfa
Zandile Gebuza <2562617@students.wits.ac.za>
2024-09-19 16:55:38 +0200

    Half-done Character Page

---
+++ scripts/sidebar.js
@@ -0,0 +1,13 @@
+//Get references to the "X" icon, the burger icon, and the sidebar
+const closeBtn = document.querySelector(".close-btn");
+const sidebarBtn = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".show-sidebar");
+
+//Add event listeners for clicking
+closeBtn.addEventListener("click", function(){
+ sidebar.style.display = "none";
+});
+
+sidebarBtn.addEventListener("click", function(){
+  sidebar.style.display = "block";
+});
