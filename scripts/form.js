c95f7aa02159f08844fd393d4a4fd5b26f3ce444
Zandile Gebuza <2562617@students.wits.ac.za>
2024-10-26 14:53:45 +0200

    Form Validation Part3

--- scripts/form.js
+++ scripts/form.js
@@ -1,16 +1,31 @@
-//Contact Form
-/*The user can contact me by writing a message into the text field from my About page.*/
+//Newsletter Subscription Form
+//The user can create a password to access the newsletter
+function submitPassword(){
+  const userPassword = document.getElementById("password").value;
 
+  //If nothing is written, the error message will appear.
+  if (userPassword.trim() === ""){
+    alert("Please enter a password.");  
+  } 
+  else{
+    //This is the response that the user will get after submitting the form.
+    alert("Password is successfully saved!");  
+
+  //Clear the text area when the message is submitted
+    document.getElementById("password").value = "";
+  }
+}
+
+//The user can contact me by writing a message into the text field from my About page.
 //This is the function for the Submit button. When it is clicked, the code will check if the user wrote something.
-function submitForm(){
+function submitMessage(){
     const userMessage = document.getElementById("message").value;
-    const responseMessage = document.getElementById("responseMessage");
 
-  //If nothing is written, the error message will appear in red. If something is written, the response will appear in purple.
+  //If nothing is written, the error message will appear.
     if (userMessage.trim() === ""){
         alert("Please enter a message.");  
     } 
-    else {
+    else{
         //This is the response that the user will get after submitting the form.
         alert("Thank you for your message!");  
 
