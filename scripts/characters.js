b3ab2abf8ad4e2de100e2954004e8045c6427f55
Zandile Gebuza <2562617@students.wits.ac.za>
2024-11-02 14:16:16 +0200

    Spelling and Grammar Edits

--- scripts/characters.js
+++ scripts/characters.js
@@ -16,7 +16,7 @@ export const characters = [
     image: "content-images/Amy.jpg",
     portrayedBy: "Melissa Fumero",
     occupation: "Sergeant",
-    personality: `Amy is a very relatable character (well, to me at least). She's a know-it-all/nerd character who loves being organised and 
+    personality: `Amy is a very relatable character (well, to me at least). She's a know-it-all character who loves being organised and 
                   doing things by the book. She's also a bit of a teacher's pet because most of the things she does is to impress the captain. 
                   Overall, Amy is a great character and I like that she is not written as a one dimensional nerd character but there are layers 
                   to her personality.`,
@@ -28,10 +28,10 @@ export const characters = [
     image: "content-images/Holt.png",
     portrayedBy: "Andre Braugher",
     occupation: "Captain",
-    personality: `Apart from Gina (on the next page) Captain Holt is my favourite character. He's a well-written, complex character with an amazing 
-                  storyline throughout the series. He is a gay, black character, and I like that the writers did not make him a stereotypically 
-                  flambouyant person nor did they make him a steroetypical "stern/scary" black man. They instead made him a funny, empathetic, 
-                  and devoted character and his interactions with other characters are always a fun watch.`,
+    personality: `Apart from Gina (on the next page) Captain Holt is my favourite character. He's a well-written, complex character with an 
+                  amazing storyline throughout the series. He is a gay, black character, and I like that the writers did not make him a 
+                  stereotypically flambouyant person nor did they make him a stereotypical "scary" black man. They instead made him a funny, 
+                  empathetic, and devoted character and his interactions with other characters are always fun to watch.`,
     quote: `"Every time someone steps up and says who they are, the world becomes a better, more interesting place" - S5 Episode 10.`,
 },
 
@@ -40,10 +40,9 @@ export const characters = [
     image: "content-images/Gina.png",
     portrayedBy: "Chelsea Peretti",
     occupation: "Assistant",
-    personality: `Gina is my number one favourite character. She's very funny, a bit narcissistic, but overall she's a good person. Gina can also 
-                  be a bit delusional at times but she exudes confidence that makes people believe the things she is saying even when she's lying. 
-                  What I love most about this character is that she is unhinged. She does and says whatever she wants and people, usually, don't 
-                  question her.`,
+    personality: `Gina is my number one favourite character. She's very funny, a bit narcissistic, but an overall good person. Gina can sometimes
+                  be a bit delusional, but she exudes confidence that makes people believe what she says, even when she's lying. What I love most 
+                  about her is that she does and says whatever she wants and people, usually, don't question her.`,
     quote: `"All men are at least 30% attracted to me. My mother cried the day I was born, because she knew she would never be better than me" - 
             S1 Episode 16.`,
 },
@@ -54,9 +53,9 @@ export const characters = [
     portrayedBy: "Joe Lo Truglio",
     occupation: "Detective",
     personality: `Charles is a really funny character. When I watch an episode of the show and he's on the screen, I just know that I will be 
-                  laughing at any moment. I also enjoy his confidence in himself because he doesn't let anyone tell what him what he can't do 
-                  even when we as the audience know that he actually can't do it. And lastly, I find his loyalty to Jake to be really sweet and 
-                  his obsession with Jake and Amy's relationship super relatable.`,
+                  laughing at any moment. I also enjoy his confidence in himself, he doesn't let anyone tell what him what he can't do even when 
+                  we as the audience know that he actually can't do it. And lastly, I find his loyalty to Jake really sweet and his 
+                  obsession with Jake and Amy's relationship super relatable.`,
     quote: `"What about me? What if something happens to Jake, and he never gets to meet my baby? I don't want to hang out with some stupid baby 
             who's never met Jake" - S3 Episode 23.`,
 },
@@ -68,7 +67,9 @@ export const characters = [
     occupation: "Detective",
     personality: `Rosa is pretty chilled character. She likes being by herself and minding her own business and it's sometimes hard for her to 
                   trust people. Rosa is also a bisexual character and after she comes out she becomes more open around her friends and family. In 
-                  general, I like how her character has grown throughout the seasons.`,
+                  general, I like how her character has grown throughout the seasons. She's not my favourite character, but I do enjoy how this 
+                  character is so different from Stephanie Beatriz's real personality and the other roles she's had. This shows that she has 
+                  range as an actor.`,
     quote: `"Two steps forward, one step  back is still one step  forward" - S6 Episode 8.`,
 },
 
